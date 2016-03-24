a64dddfcb9ae3989a9cb250d91986b0bb6dfd93a
abzcoding <abzcoding@gmail.com>
2016-03-24 13:27:21 +0430

    fixed cuckoo test

--- tests/test_cuckoo.py
+++ tests/test_cuckoo.py
@@ -1,19 +1,18 @@
 """docstring"""
-import requests
 from bs4 import BeautifulSoup
 
 
-soup = BeautifulSoup(requests.get("http://172.16.201.182:8000/analysis/17").
-                     text, 'html.parser')
-information = soup.find(id="information")
-print("--------------informations--------------")
-for ch in information.find(class_="panel-body"):
-    if len(ch.string.strip()) > 0:
+with open("tests/test_cuckoo.html") as f:
+    soup = BeautifulSoup(f.read(), 'html.parser')
+    information = soup.find(id="information")
+    print("--------------informations--------------")
+    for ch in information.find(class_="panel-body"):
+        if len(ch.string.strip()) > 0:
+            print(ch.string.strip())
+    all_sig = soup.find(id="signatures")
+    print("----------------warnings----------------")
+    for ch in all_sig.find_all("div", class_="alert-warning"):
         print(ch.string.strip())
-all_sig = soup.find(id="signatures")
-print("----------------warnings----------------")
-for ch in all_sig.find_all("div", class_="alert-warning"):
-    print(ch.string.strip())
-print("-----------------errors-----------------")
-for ch in all_sig.find_all("div", class_="alert-danger"):
-    print(ch.string.strip())
+    print("-----------------errors-----------------")
+    for ch in all_sig.find_all("div", class_="alert-danger"):
+        print(ch.string.strip())
